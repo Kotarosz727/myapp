dc3239a03a561932e3a8b5db3266a897ee85d877
Kotaro Suzuki <kotarosz727@gmail.com>
2019-04-09 18:54:34 +0900

    add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')                     
+    base_title = "Favorite Movie"  
+    if page_title. blank?                              
+      base_title                                     
+    else 
+      page_title               
+    end
+  end
+
 end

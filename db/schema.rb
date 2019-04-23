f17927463e6c7903a8eca6e331ffbc0bf5b7ce32
Kotaro Suzuki <kotarosz727@gmail.com>
2019-04-23 18:53:15 +0900

    add admin culumn to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_093701) do
+ActiveRecord::Schema.define(version: 2019_04_23_092603) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,9 @@ ActiveRecord::Schema.define(version: 2019_04_08_093701) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "provider"
+    t.string "uid"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

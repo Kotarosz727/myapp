6217c69ca9e72ca78623b803a10c582ebf71ed13
Kotaro Suzuki <kotarosz727@gmail.com>
2019-04-24 12:59:56 +0900

    revise header

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
 class Play < ApplicationRecord
+
+  validates :description, length: { maximum: 400 }
   belongs_to :user
   belongs_to :category
   has_one_attached :image
-
   scope :random, -> { order('RANDOM()') }
-  # scope :related_plays, ->(category, play) {
-  # category.play.
-  # where.not(id: play.id).
-  # limit(4).
-  # distinct
-  # }
+
 end

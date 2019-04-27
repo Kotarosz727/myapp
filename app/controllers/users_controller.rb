c74ac1eec4ce527e7af1bce07f99b4ab23f82aa6
Kotaro Suzuki <kotarosz727@gmail.com>
2019-04-27 12:48:15 +0900

    revise User contoroller where.not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,12 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.all.includes(avatar_attachment: [:blob])
+    @users = User.all.includes(avatar_attachment: [:blob]).where.not(id: current_user.id)
   end
 
   def show
     @user = User.find(params[:id])
     @plays = @user.plays.all.includes(image_attachment: [:blob])
   end
-
   
 end

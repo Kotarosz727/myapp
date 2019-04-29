735fdf4821227adc5fee9b1e1c6ba02da6156196
Kotaro Suzuki <kotarosz727@gmail.com>
2019-04-29 21:20:34 +0900

    add favorite function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,24 @@
 class FavoritesController < ApplicationController
+ 
+  before_action :set_play
 
   def create
     @favorite = Favorite.create(user_id: current_user.id, play_id: params[:play_id])
     @favorites = Favorite.where(play_id: params[:play_id])
-    if @favorite.save
-      flash[:success] = "お気に入りしました"
-      redirect_to root_path
-    end
+    @play.reload
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id, play_id: params[:play_id])
     favorite.destroy
+    head :no_content
     @favorites = Favorite.where(play_id: params[:play_id]) 
+    
   end  
+
+  def set_play
+    @play = Play.find(params[:play_id])
+  end
 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :avatar, presence: false
   has_many :plays, dependent: :destroy
   has_many :favorites
+  has_many :fav_movies, through: :favorites, source: :play
   has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
